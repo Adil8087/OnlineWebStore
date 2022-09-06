8a598aadd2c5a2619db085d5acd10c19f086c60b
Adil8087 <pathanadil786.ap@gmail.com>
2022-09-06 21:40:26 +0530

    Spring boot application created with all dependencies and ready made view

---
+++ src/test/java/com/OnWeb/demo/DemoOnlineStoreApplicationTest.java
@@ -0,0 +1,14 @@
+package com.OnWeb.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DemoOnlineStoreApplicationTest {
+
+	@Test
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
